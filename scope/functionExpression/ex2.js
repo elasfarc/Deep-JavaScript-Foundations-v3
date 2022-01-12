8f81d61e2da112d0d7e250a0b55a4bca81accb44
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-01-12 12:12:22 +0200

    [scope][function-expressions] Add #printRecords(arrow function)

---
+++ scope/functionExpression/ex2.js
@@ -0,0 +1,51 @@
+const printRecords = (recordIds) =>
+  recordIds
+    .map((id) => studentRecords.find((record) => record.id === id))
+    .sort(({ name: name1 }, { name: name2 }) =>
+      name1 > name2 ? 1 : name1 < name2 ? -1 : 0
+    )
+    .map(
+      ({ name, id, paid }) => `${name} (${id}): ${paid ? "Paid" : "Not Paid"}`
+    )
+    .forEach((record) => console.log(record));
+
+// ********************************
+
+var currentEnrollment = [410, 105, 664, 375];
+
+var studentRecords = [
+  { id: 313, name: "Frank", paid: true },
+  { id: 410, name: "Suzy", paid: true },
+  { id: 709, name: "Brian", paid: false },
+  { id: 105, name: "Henry", paid: false },
+  { id: 502, name: "Mary", paid: true },
+  { id: 664, name: "Bob", paid: false },
+  { id: 250, name: "Peter", paid: true },
+  { id: 375, name: "Sarah", paid: true },
+  { id: 867, name: "Greg", paid: false },
+];
+
+printRecords(currentEnrollment);
+// console.log("----");
+// currentEnrollment = paidStudentsToEnroll();
+// printRecords(currentEnrollment);
+// console.log("----");
+// remindUnpaid(currentEnrollment);
+
+/*
+	Bob (664): Not Paid
+	Henry (105): Not Paid
+	Sarah (375): Paid
+	Suzy (410): Paid
+	----
+	Bob (664): Not Paid
+	Frank (313): Paid
+	Henry (105): Not Paid
+	Mary (502): Paid
+	Peter (250): Paid
+	Sarah (375): Paid
+	Suzy (410): Paid
+	----
+	Bob (664): Not Paid
+	Henry (105): Not Paid
+*/
